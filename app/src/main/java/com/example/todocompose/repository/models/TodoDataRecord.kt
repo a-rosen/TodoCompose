ccdebb0a60c419bcb0f12ed4cf25172ea99aaec8
a-rosen <annierrosen@gmail.com>
2023-11-03 13:01:52 -0400

    WIP database

--- app/src/main/java/com/example/todocompose/repository/models/TodoDataRecord.kt
+++ app/src/main/java/com/example/todocompose/repository/models/TodoDataRecord.kt
@@ -1,7 +1,5 @@
 package com.example.todocompose.repository.models
 
-import com.example.todocompose.database.models.TodoEntity
-
 data class TodoDataRecord(
     val id: Long,
     val name: String,
